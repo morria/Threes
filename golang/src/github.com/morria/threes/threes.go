d1415b92bce5981c0ed400a56cff44c9faab2185
Andrew Morrison <asm@theory.stanford.edu>
2017-01-01 21:27:52 -0500

    go version

---
+++ golang/src/github.com/morria/threes/threes.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "math"
+  "math/rand"
+  "time"
+  "sort"
+)
+
+type Strategy [][]bool
+type StrategySet [][][]bool
+
+type StrategyEvaluation struct {
+  strategySet StrategySet
+  average float64
+  nonLossRatios []float64
+}
+
+/**
+ * Print out a strategy and its average sum to
+ * STDOUT
+ */
+func printEvaluation(eval StrategyEvaluation) {
+  for _,strategy := range eval.strategySet {
+    for i := range strategy {
+      str := []string{}
+      for j := range strategy[i] {
+        if strategy[i][j] {
+          str = append(str, "1")
+        } else {
+          str = append(str, "0")
+        }
+      }
+      fmt.Printf("[%s]\n", strings.Join(str, ", "))
+    }
+    fmt.Print("\n")
+  }
+  fmt.Printf("= %f\n", eval.average)
+  nonLossRatioStrings := []string{}
+  for _,ratio := range eval.nonLossRatios {
+    nonLossRatioStrings = append(nonLossRatioStrings, fmt.Sprintf("%.3f", ratio * 100.0))
+  }
+  fmt.Printf("  [%s]\n", strings.Join(nonLossRatioStrings, ", "))
+  fmt.Printf("\n")
+}
+
+/**
+ * Roll the given number of dice, getting them
+ * back in a sorted order
+ */
+func roll(dice_count int) []int {
+  dice := []int{}
+  for i := 0; i < dice_count; i++ {
+    dice = append(dice, rand.Intn(6) + 1)
+  }
+  sort.Ints(dice)
+  return dice
+}
+
+/**
+ * Get the sum of a list of dice
+ */
+func diceSum(dice []int) int {
+  sum := 0
+  for i,d := range dice {
+    if d != 3 {
+      sum += dice[i]
+    }
+  }
+  return sum
+}
+
+/**
+ * Evaluate a given strategy matrix for the given
+ * number of iterations, gettings its average sum
+ * back.
+ */
+func evaluateStrategy(strategySet StrategySet, iterations int, channel chan StrategyEvaluation) {
+  globalSum := 0
+  nonLossRatios := []float64{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}
+  for iteration := 0; iteration < iterations; iteration++ {
+    keep := []int{}
+    for len(keep) < 5 {
+
+      // Roll the dice
+      dice := roll(5 - len(keep))
+
+      // Search over the dice for ones to keep
+      kept := false
+      for _,d := range dice {
+
+        // Choose the strategy from the strategy set
+        strategy := strategySet[len(strategySet)-1]
+        keepSum := diceSum(keep)
+        if len(strategySet) > keepSum {
+          strategy = strategySet[keepSum]
+        }
+
+        // Choose to keep or discard dice based on
+        // the chosen strategy
+        if strategy[len(keep)][d-1] {
+          keep = append(keep, d)
+          kept = true
+        }
+      }
+
+      // If we didn't choose any, keep the lowest
+      // valued dice
+      if !kept {
+        keep = append(keep, dice[0])
+      }
+    }
+    sum := diceSum(keep)
+    for i := 0; i < len(nonLossRatios); i++ {
+      if sum <= i {
+        nonLossRatios[i]++
+      }
+    }
+    globalSum += sum
+  }
+  average := (float64(globalSum)/float64(iterations))
+  for i := 0; i < len(nonLossRatios); i++ {
+    nonLossRatios[i] /= float64(iterations)
+  }
+  channel<-StrategyEvaluation{strategySet, average, nonLossRatios}
+}
+
+/**
+ * Produce a strategy from an integer
+ */
+func strategyFromInt(s int) Strategy {
+    strategy := Strategy{}
+    strategy = append(strategy, []bool{(s&(1<<0))>0, false,        true, false, false, false})
+    strategy = append(strategy, []bool{(s&(1<<1))>0, false,        true, false, false, false})
+    strategy = append(strategy, []bool{(s&(1<<2))>0, (s&(1<<3))>0, true, false, false, false})
+    strategy = append(strategy, []bool{(s&(1<<4))>0, (s&(1<<5))>0, true, false, false, false})
+    strategy = append(strategy, []bool{(s&(1<<6))>0, (s&(1<<7))>0, true, false, false, false})
+    return strategy
+}
+
+/**
+ * Produce a set of strategies from a set of
+ * integers
+ */
+func strategySetFromIntSet(s []int) StrategySet {
+  strategySet := StrategySet{}
+  for i := range s {
+    strategySet = append(strategySet, strategyFromInt(s[i]))
+  }
+  return strategySet
+}
+
+/**
+ * Generate strategies and evaluate them, printing
+ * out the best ones as we find them
+ */
+func main() {
+
+  // Choose a random-ish seed
+  rand.Seed(time.Now().UTC().UnixNano())
+
+  // Create a channel for which strategy evaluations
+  // may be returned
+  channel := make(chan StrategyEvaluation, 4)
+
+  // Iterate over all possible strategies to find the one
+  // that produces the lowest average values
+  strategyCount := 0
+  for s0 := 0; s0 <= (int(math.Pow(2, 8)) - 1); s0++ {
+    for s1 := 0; s1 <= (int(math.Pow(2, 8)) - 1); s1++ {
+      go evaluateStrategy(strategySetFromIntSet([]int{s0, s0, s0, s1}), 1000, channel)
+      strategyCount++
+    }
+  }
+
+  // We're going to hunt for the lowest average value
+  minEval := StrategyEvaluation{strategySetFromIntSet([]int{}), 30.0, []float64{}}
+  for i := 0; i < strategyCount; i++ {
+  // for s := 0; s <= (int(math.Pow(2, 10)) - 1); s++ {
+    eval := <-channel
+    if eval.average < minEval.average {
+      minEval = eval
+    }
+    if i%100 == 0 {
+      fmt.Print(".")
+    }
+  }
+  fmt.Print("\n")
+  printEvaluation(minEval)
+}
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from random import randint
 import sys
 
+# Definition of a game of Threes
 class Threes(object):
     def __init__(self):
         self.kept = [];
@@ -15,6 +16,8 @@ class Threes(object):
         self.kept.extend(keep)
     def keptLen(self):
         return len(self.kept)
+    def keptSum(self):
+        return sum(self.kept)
     def isComplete(self):
         return len(self.kept) == 5
     def sum(self):
@@ -24,8 +27,8 @@ class Threes(object):
                 sum += i
         return sum
 
-# Keep all dice as they are initially thrown
-class StrategyKeepAll(object):
+# Definition of a strategy that plays Threes
+class Strategy(object):
     def __init__(self):
         self.game = Threes()
     def reset(self):
@@ -38,55 +41,14 @@ class StrategyKeepAll(object):
             self.round()
         return self.game.sum()
 
-# Only keep threes or the lowest valued
-# dice we have
-class StrategyKeepOnlyThrees(object):
-    def __init__(self):
-        self.game = Threes()
-    def reset(self):
-        self.game = Threes()
-    def round(self):
-        dice = self.game.roll()
-        keep = []
-        for i in dice:
-            if i == 3:
-                keep.append(i)
-        if len(keep) < 1:
-            keep.append(min(dice))
-        self.game.keep(keep)
-    def sum(self):
-        while not self.game.isComplete():
-            self.round()
-        return self.game.sum()
-
-# Only keep threes/ones or the lowest valued
-# dice we have
-class StrategyKeepThreesOnes(object):
-    def __init__(self):
-        self.game = Threes()
-    def reset(self):
-        self.game = Threes()
-    def round(self):
-        dice = self.game.roll()
-        keep = []
-        for i in dice:
-            if i == 3 or i == 1:
-                keep.append(i)
-        if len(keep) < 1:
-            keep.append(min(dice))
-        self.game.keep(keep)
-    def sum(self):
-        while not self.game.isComplete():
-            self.round()
-        return self.game.sum()
-
-# Only keep threes/ones/twos or the lowest
-class StrategyMatrix(object):
+# A strategy defined by a 2D matrix with axes being
+# the number of 'keeps' and a boolean for each of the
+# six dice, choosing if they should be kept at the
+# given keep-length.
+class StrategyMatrix(Strategy):
     def __init__(self, matrix):
-        self.game = Threes()
+        Strategy.__init__(self)
         self.matrix = matrix;
-    def reset(self):
-        self.game = Threes()
     def round(self):
         dice = self.game.roll()
         keep = []
@@ -96,63 +58,114 @@ class StrategyMatrix(object):
         if (len(keep) < 1):
             keep.append(min(dice))
         self.game.keep(keep)
-    def sum(self):
-        while not self.game.isComplete():
-            self.round()
-        return self.game.sum()
 
-# Test a given strategy over many iterations,
-# averaging its results
-class TestStrategy(object):
-    def __init__(self, strategy, iterations):
-        self.strategy = strategy
-        self.iterations = iterations
-    def avg(self):
-        sum = 0
-        for i in range(0, self.iterations):
-            self.strategy.reset()
-            sum += self.strategy.sum()
-        return sum/(self.iterations * 1.0)
+class Strategy3D(Strategy):
+    def __init__(self, matrix):
+        Strategy.__init__(self)
+        self.matrix = matrix;
+    def round(self):
+        keep = []
+        kept_sum= self.game.keptSum()
+        dice = self.game.roll()
+        for i in dice:
+            matrix = self.matrix[kept_sum] if len(self.matrix) > kept_sum else self.matrix[-1]
+            if matrix[self.game.keptLen() + len(keep)][i-1]:
+                kept_sum += i
+                keep.append(i)
+        if (len(keep) < 1):
+            keep.append(min(dice))
+        self.game.keep(keep)
 
-def p(m, avg):
-  for i in range(5):
-    print(str(map(lambda x: int(x), m[i])), end="\n")
+# An example strategy where all dice are always kept
+class StrategyKeepAll(StrategyMatrix):
+    def __init__(self):
+        matrix = []
+        matrix.append([True, True, True, True, True, True])
+        matrix.append([True, True, True, True, True, True])
+        matrix.append([True, True, True, True, True, True])
+        matrix.append([True, True, True, True, True, True])
+        matrix.append([True, True, True, True, True, True])
+        StrategyMatrix.__init__(self)
+
+# An example strategy where only threes are kept
+class StrategyKeepOnlyThrees(StrategyMatrix):
+    def __init__(self):
+        matrix = []
+        matrix.append([False, False, True, False, False, False])
+        matrix.append([False, False, True, False, False, False])
+        matrix.append([False, False, True, False, False, False])
+        matrix.append([False, False, True, False, False, False])
+        matrix.append([False, False, True, False, False, False])
+        StrategyMatrix.__init__(self)
+
+def evaluateStrategy(strategy, iterations):
+    sum = 0
+    wins = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(0, iterations):
+        strategy.reset()
+        s = strategy.sum()
+        sum += s
+        for i in range(10):
+            if s <= i:
+                wins[i] += 1
+    return (
+            sum/(iterations*1.0),
+            map(lambda x: round((x/(iterations*1.0))*100.0, 2), wins)
+            )
+
+def p(m, avg, win_ratios):
+  for s in m:
+    for i in range(5):
+      print(str(map(lambda x: int(x), s[i])), end="\n")
+    print("")
   print(' = ' + str(avg), end="\n")
-
-# s = []
-# s.append([False, False, True, False, False, False])
-# s.append([False, False, True, False, False, False])
-# s.append([True, True, True, False, False, False])
-# s.append([True, True, True, False, False, False])
-# s.append([True, True, True, False, False, False])
-# print(str(500000) + "\t" + str(TestStrategy(StrategyMatrix(s), 500000).avg()))
-# for i in range(1, 100):
-#  print(str(i*1000) + "\t" + str(TestStrategy(StrategyMatrix(s), i*1000).avg()))
-#  sys.stdout.flush()
-
-# 6.3805368
-# s = []
-# s.append([0, 0, 1, 0, 0, 0])
-# s.append([1, 0, 1, 0, 0, 0])
-# s.append([1, 0, 1, 0, 0, 0])
-# s.append([1, 1, 1, 0, 0, 0])
-# s.append([1, 1, 1, 0, 0, 0])
-# avg = TestStrategy(StrategyMatrix(s), 5000000).avg()
-# print(str(avg))
-# sys.exit(0)
+  print(win_ratios, end="\n\n")
 
 min_s = 0;
 min_avg = 100;
-for m in range(0, 2**6):
-    s = []
-    s.append([m&(1<<2)>0, False,      True, False,      False, False])
-    s.append([m&(1<<1)>0, m&(1<<5)>0, True, False,      False, False])
-    s.append([m&(1<<0)>0, m&(1<<4)>0, True, False,      False, False])
-    s.append([True,       m&(1<<3)>0, True, False,      False, False])
-    s.append([True,       True,       True, m&(1<<6)>0, False, False])
-    avg = TestStrategy(StrategyMatrix(s), 500000).avg()
-    if avg < min_avg:
-        min_avg = avg
-        min_s = s
-        p(min_s, avg)
+win_ratio = 0;
+for m in range(0, 2**7):
+
+    s0 = []
+    s0.append([False,            False,            True, False, False, False])
+    s0.append([True,             False,            True, False, False, False])
+    s0.append([True,             False,            True, False, False, False])
+    s0.append([True,             True,             True, False, False, False])
+    s0.append([True,             True,             True, False, False, False])
+
+    s1 = []
+    s1.append([False,            False,            True, False, False, False])
+    s1.append([True,             False,            True, False, False, False])
+    s1.append([True,             False,            True, False, False, False])
+    s1.append([True,             True,             True, False, False, False])
+    s1.append([True,             True,             True, False, False, False])
+
+    s2 = []
+    s2.append([False,            False,            True, False, False, False])
+    s2.append([(m&(1<<3)>0),     False,            True, False, False, False])
+    s2.append([(m&(1<<2)>0),     False,            True, False, False, False])
+    s2.append([(m&(1<<1)>0),     False,            True, False, False, False])
+    s2.append([(m&(1<<0)>0),     True,             True, False, False, False])
+
+    s3 = []
+    s3.append([False,            False,            True, False, False, False])
+    s3.append([(m&(1<<7)>0),     False,            True, False, False, False])
+    s3.append([(m&(1<<6)>0),     False,            True, False, False, False])
+    s3.append([(m&(1<<5)>0),     False,            True, False, False, False])
+    s3.append([(m&(1<<4)>0),     False,            True, False, False, False])
+
+    s4 = []
+    s4.append([False, False, True, False, False, False])
+    s4.append([False, False, True, False, False, False])
+    s4.append([False, False, True, False, False, False])
+    s4.append([False, False, True, False, False, False])
+    s4.append([False, False, True, False, False, False])
+
+    (avg, win_ratios) = evaluateStrategy(Strategy3D([s0, s1, s3, s4]), 100000)
+    if win_ratios[4] > win_ratio:
+        win_ratio = win_ratios[4]
+#    if avg < min_avg:
+#         min_avg = avg
+        min_s = [s0, s1, s2, s3]
+        p(min_s, avg, win_ratios)
         sys.stdout.flush()
